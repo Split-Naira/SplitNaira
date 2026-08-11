4e11c380c5cf728eba1b7a30088f3f951f970009
chinaza007 <ibitayogodsmiracle@gmail.com>
2026-08-11 13:02:57 +0100

    fix migration dry-run index idempotency

--- backend/src/migrations/1760000000005-AddUniqueIndexTransactionHash.ts
+++ backend/src/migrations/1760000000005-AddUniqueIndexTransactionHash.ts
@@ -4,10 +4,10 @@ export class AddUniqueIndexTransactionHash1760000000005 implements MigrationInte
   name = "AddUniqueIndexTransactionHash1760000000005";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE UNIQUE INDEX "IDX_transactions_tx_hash" ON "transactions" ("txHash")`);
+    await queryRunner.query(`CREATE UNIQUE INDEX IF NOT EXISTS "IDX_transactions_tx_hash" ON "transactions" ("txHash")`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP INDEX "public"."IDX_transactions_tx_hash"`);
+    await queryRunner.query(`DROP INDEX IF EXISTS "public"."IDX_transactions_tx_hash"`);
   }
 }
